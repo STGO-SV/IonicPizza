b1d48707d14e48940b1d964ca967d20be0449bc7
Samuel <samuelalves681@gmail>
2021-12-29 15:39:29 -0300

    animation cart icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,25 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 export class HeaderComponent implements OnInit {
   @Input() title: string;
   @Input() categories: any[];
-  public dropdown = false;
   @ViewChild('productbtn', { read: ElementRef }) productbtn: ElementRef;
-  constructor() {}
+  @ViewChild('cartbtnweb', { read: ElementRef }) cartBtnWeb: ElementRef;
+  @ViewChild('cartbtnmobile', { read: ElementRef }) cartBtnMobile: ElementRef;
 
-  ngOnInit() {}
+  public dropdown = false;
+  public cartCount = 0;
+  constructor(
+    private animationCtrl: AnimationController,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit() {
+    this.cartService.getCartCount().subscribe((value) => {
+      if (value > 0) {
+        this.animateCart();
+      }
+      this.cartCount = value;
+    });
+  }
 
   hideDropdown(event) {
     const xTouch = event.clientX;
@@ -30,5 +46,29 @@ export class HeaderComponent implements OnInit {
     ) {
       this.dropdown = false;
     }
+  }
+
+  animateCart() {
+    const keyframes = [
+      { offset: 0, transform: 'scale(1)' },
+      { offset: 0.5, transform: 'scale(1.2)' },
+      { offset: 0.8, transform: 'scale(0.9)' },
+      { offset: 1, transform: 'scale(1)' },
+    ];
+
+    const cartAnimationWeb = this.animationCtrl
+      .create('web')
+      .addElement(this.cartBtnWeb.nativeElement)
+      .duration(600)
+      .keyframes(keyframes);
+
+    cartAnimationWeb.play();
+
+    const cartAnimationMobile = this.animationCtrl
+      .create('mobile')
+      .addElement(this.cartBtnMobile.nativeElement)
+      .duration(600)
+      .keyframes(keyframes);
+    cartAnimationMobile.play();
   }
 }
