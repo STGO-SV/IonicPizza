084dbcc6ae04352b42cf4b1b00893d8e9cca6ce4
Samuel <samuelalves681@gmail>
2021-12-28 22:08:50 -0300

    initial template/ts menu

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { isPlatform } from '@ionic/angular';
+
+import categoryData from '../../../assets/company/categories.json';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.page.scss'],
 })
 export class MenuPage implements OnInit {
+  public menuItems = [
+    {
+      title: 'Home',
+      icon: 'home',
+      path: '/',
+    },
+    {
+      title: 'Products',
+      icon: 'list',
+      path: '/products',
+      children: categoryData,
+    },
+    {
+      title: 'About',
+      icon: 'information',
+      path: '/about',
+    },
+  ];
 
-  constructor() { }
+  public title = 'Home';
+  constructor() {}
 
   ngOnInit() {
+    const headerHeight = isPlatform('ios') ? 44 : 56;
+    document.documentElement.style.setProperty(
+      '--header-height',
+      `${headerHeight}px`
+    );
   }
 
+  setTitle(title: string) {
+    this.title = title;
+  }
 }
