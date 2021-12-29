746208f3d951a07c70238e045ccf24f4c414eb7e
Samuel <samuelalves681@gmail>
2021-12-29 14:36:00 -0300

    cartService

--- src/app/pages/products/products.page.ts
+++ src/app/pages/products/products.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService, Product } from 'src/app/services/cart.service';
+import productData from '../../../assets/company/menu.json';
+import categoryData from '../../../assets/company/categories.json';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
-
-  constructor() { }
+  public products = [];
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      this.filterProducts(params.category);
+    });
   }
 
+  filterProducts(category = null) {
+    if (!category) {
+      this.products = productData;
+    } else {
+      const cat = categoryData.filter((item) => item.slug === category)[0];
+      this.products = productData.filter((p) => p.category === cat.id);
+    }
+  }
+
+  addProduct(product: Product) {
+    this.cartService.addProducts(product);
+  }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Product {
+  id: string;
+  price: string;
+  img: string;
+  title: string;
+  desc: string;
+  category: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  private cart = {};
+  private cartItems = new BehaviorSubject(0);
+  constructor() {}
+
+  addProducts(product: Product) {
+    if (!this.cart[product.id]) {
+      this.cart[product.id] = {
+        amount: 1,
+        ...product,
+      };
+    } else {
+      this.cart[product.id].amount += 1;
+    }
+
+    this.cartItems.next(this.cartItems.value + 1);
+  }
+
+  getCartCount() {
+    return this.cartItems.asObservable();
+  }
+
+  getCart() {
+    const cartItems = [];
+    for (const [key, value] of Object.entries(this.cart)) {
+      cartItems.push(value);
+    }
+
+    return cartItems;
+  }
+}
